2e74ca70e37305d8f67b0e86514f14e1d94b37b6
ILzat <169165812+Fanatic033@users.noreply.github.com>
2024-10-26 01:53:19 +0600

    'Добавил все цвета с макета' (#1)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Button } from '@/components/ui/button';
 
 export const App: React.FC = () => {
-  return <Button>Initial app</Button>;
+  return (
+    <>
+    </>
+  );
 };
