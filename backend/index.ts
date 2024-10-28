4955e021d9ad8ae8df390238a715f16944bee81f
ilzat <ilzat.sadykov.2006@bk.ru>
2024-10-28 21:13:33 +0600

    'Add partner card component with backend support and integrate react-fast-marquee library
     - Created Partner card component to display partner details
     - Set up backend routes and schema for Partner data
     - Integrated react-fast-marquee library for smooth scrolling effect
    '

--- backend/index.ts
+++ backend/index.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import config from './config';
 import { ranksRouter } from './routers/ranks';
 import { usersRouter } from './routers/users';
+import { partnersRouter } from './routers/partners';
 
 const app = express();
 const port = 8000;
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use('/users', usersRouter);
 app.use('/ranks', ranksRouter);
+app.use('/partners', partnersRouter);
 
 const run = async () => {
   await mongoose.connect(config.database);
