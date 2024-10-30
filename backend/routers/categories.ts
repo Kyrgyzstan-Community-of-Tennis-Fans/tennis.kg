1ae6e6a0b64dadc5c55249d46c3f10035ee45dc6
nenahui <153425875+nenahui@users.noreply.github.com>
2024-10-30 23:06:05 +0600

    TC#11 - Categories (#9)
    * implement categories crud on backend and frontend side
    
    * implement crud on backend side for categories
    
    * fixed bugs and improved responsive
    
    ---------
    
    Co-authored-by: Kanat Sydykov <kanatsydykov@MacBook-Air-Kanat.local>

--- backend/routers/categories.ts
+++ backend/routers/categories.ts
@@ -34,3 +34,58 @@ categoriesRouter.post('/', auth, permit('admin'), async (req, res, next) => {
     return next(error);
   }
 });
+
+categoriesRouter.delete('/:id', auth, permit('admin'), async (req, res, next) => {
+  try {
+    const rank = await Category.findById(req.params.id);
+
+    if (!rank) {
+      return res.status(404).send({ message: 'Category not found' });
+    }
+
+    await rank.deleteOne();
+
+    return res.send({ message: 'Category deleted' });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+categoriesRouter.put('/:id', auth, permit('admin'), async (req, res, next) => {
+  try {
+    const { name } = req.body;
+
+    const rank = await Category.findById(req.params.id);
+
+    const isExist = await Category.findOne({ name });
+
+    if (isExist) {
+      return res.status(400).send({ message: 'Category already exists' });
+    }
+
+    if (!rank) {
+      return res.status(404).send({ message: 'Category not found' });
+    }
+
+    rank.name = name;
+    await rank.save();
+
+    return res.send(rank);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+categoriesRouter.get('/:id', async (req, res, next) => {
+  try {
+    const rank = await Category.findById(req.params.id);
+
+    if (!rank) {
+      return res.status(404).send({ message: 'Category not found' });
+    }
+
+    return res.send(rank);
+  } catch (error) {
+    return next(error);
+  }
+});
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,47 @@
+import { useAppSelector } from '@/app/hooks';
+import { Error404 } from '@/components/Errors/Error404';
+import { Layout } from '@/components/Layout';
+import ProtectedRoute from '@/components/ProtectedRouter/ProtectedRouter';
 import { Toaster } from '@/components/ui/sonner';
+import { Category } from '@/features/category/category';
 import Partners from '@/features/partners/Partners';
 import { ForgotPassword } from '@/features/users/ForgotPassword';
 import { Login } from '@/features/users/Login';
 import { Register } from '@/features/users/Register';
 import { ResetPassword } from '@/features/users/ResetPassword';
+import { selectUser } from '@/features/users/usersSlice';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 export const App: React.FC = () => {
+  const user = useAppSelector(selectUser);
+
   return (
-    <>
+    <Layout>
       <Routes>
         <Route path={'/login'} element={<Login />} />
         <Route path={'/register'} element={<Register />} />
         <Route path={'/reset-password/:token'} element={<ResetPassword />} />
         <Route path={'/forgot-password'} element={<ForgotPassword />} />
-        <Route path={'/partners'} element={<Partners />} />
+        <Route
+          path={'/partners'}
+          element={
+            <ProtectedRoute isAllowed={user && user.role === 'admin'}>
+              <Partners />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path={'/category'}
+          element={
+            <ProtectedRoute isAllowed={user && user.role === 'admin'}>
+              <Category />
+            </ProtectedRoute>
+          }
+        />
+        <Route path={'*'} element={<Error404 />} />
       </Routes>
       <Toaster />
-    </>
+    </Layout>
   );
 };
