4fc3d27d38e970d07c4c851481fec4fb910b29ca
Kanat Sydykov <kanatsydykov@MacBook-Air-Kanat.local>
2024-10-30 21:15:04 +0600

    implement crud on backend side for categories

--- backend/index.ts
+++ backend/index.ts
@@ -9,7 +9,7 @@ import { partnersRouter } from './routers/partners';
 const app = express();
 const port = 8000;
 
-app.use(cors(config.corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/users', usersRouter);
