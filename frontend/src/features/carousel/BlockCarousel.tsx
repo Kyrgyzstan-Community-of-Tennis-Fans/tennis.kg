6b62fae7018cc7432404718b2188f41b16f0a95d
Gordus Alexander <aldrmiller@gmail.com>
2024-11-01 22:22:50 +0600

    TC#3-Create-Responsiv-Carousel

--- frontend/src/features/carousel/BlockCarousel.tsx
+++ frontend/src/features/carousel/BlockCarousel.tsx
@@ -54,7 +54,7 @@ export const BlockCarousel = () => {
     <>
       <div>
         <div className='mt-[52px] text-center md:mt-[157px]'>
-          <h1 className='text-cr-black text-[20px] font-bold  uppercase px-5 mb-[25px] md:text-[64px] md:mb-[16px]'>
+          <h1 className='text-cr-black text-[20px] font-bold uppercase mb-[25px] md:text-[64px] md:mb-[16px]'>
             Кыргызстанское сообщество любителей тенниса
           </h1>
           <p className='hidden text-[#808080] md:block text-[36px] font-medium pb-[56px]'>
