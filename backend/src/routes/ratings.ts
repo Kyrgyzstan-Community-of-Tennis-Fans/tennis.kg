5190d6b27b3b3be0b4261a8964110415b155a0f2
Kanat Sydykov <kanatsydykov@MacBook-Air-Kanat.local>
2024-11-07 21:08:00 +0600

    fix bugs and implement responsive

--- backend/src/routes/ratings.ts
+++ backend/src/routes/ratings.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import { createRating, fetchRatings } from '../controllers/ratings';
+import { createRating, deleteRating, fetchRatings } from '../controllers/ratings';
 
 export const ratingsRouter = express.Router();
 
 ratingsRouter.get('/', fetchRatings);
 ratingsRouter.post('/', createRating);
+ratingsRouter.delete('/:id', deleteRating);
