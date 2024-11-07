d354c4adabf7b5101c048e0488ec776f5c7f7849
ilzat <ilzat.sadykov.2006@bk.ru>
2024-11-07 19:50:36 +0600

    'change tabs styles'

--- frontend/src/pages/AdminPanel.tsx
+++ frontend/src/pages/AdminPanel.tsx
@@ -5,20 +5,24 @@ import { AdminPaneBlockCarousel } from '@/features/carousel/AdminPaneBlockCarous
 import { Layout } from '@/components/Layout';
 import RatingMembersAdminList from '@/features/mainRatingMembers/RatingMembersAdmin/RatingMembersAdminList';
 import { AdminNews } from '@/features/news/containers/AdminNews/AdminNews';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 
 const AdminPanel = () => {
   return (
     <>
       <Layout>
         <h1 className={'text-center text-2xl sm:text-4xl mb-8 font-semibold'}>Панель Администратора</h1>
-        <Tabs defaultValue='partners'>
-          <TabsList className='grid w-full md:grid-cols-5 sm:grid-cols-5 xs:grid-cols-5 xs:mb-10 mb-36 '>
-            <TabsTrigger value='partners'>Партнеры</TabsTrigger>
-            <TabsTrigger value='category'>Категории</TabsTrigger>
-            <TabsTrigger value='carousel'>Карусель</TabsTrigger>
-            <TabsTrigger value='rating'>Рейтинг</TabsTrigger>
-            <TabsTrigger value='news'>Новости</TabsTrigger>
-          </TabsList>
+        <Tabs defaultValue='partners' orientation={'vertical'}>
+          <ScrollArea className={'max-w-max pb-3'}>
+            <TabsList className='flex items-center gap-1'>
+              <TabsTrigger value='partners'>Партнеры</TabsTrigger>
+              <TabsTrigger value='category'>Категории</TabsTrigger>
+              <TabsTrigger value='carousel'>Карусель</TabsTrigger>
+              <TabsTrigger value='rating'>Рейтинг</TabsTrigger>
+              <TabsTrigger value='news'>Новости</TabsTrigger>
+            </TabsList>
+            <ScrollBar orientation={'horizontal'} />
+          </ScrollArea>
           <TabsContent value='partners'>
             <AdminPartners />
           </TabsContent>
