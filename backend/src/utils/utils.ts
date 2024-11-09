44b1b4efcdb329a4b0e574585af056eb4f1a0390
Kanat Sydykov <kanatsydykov@MacBook-Air-Kanat.local>
2024-11-09 07:48:08 +0600

    improved reset password mail design

--- backend/src/utils/utils.ts
+++ backend/src/utils/utils.ts
@@ -1,4 +1,7 @@
+import * as handlebars from 'handlebars';
+import * as fs from 'node:fs';
 import nodemailer from 'nodemailer';
+import path from 'path';
 import { UserFields } from '../../types';
 import { RatingMember } from '../model/RatingMember';
 import { Response } from 'express';
@@ -15,6 +18,17 @@ import { Response } from 'express';
 // Пока можно и так оставить
 
 export const sendMail = async (user: UserFields, token: string) => {
+  const __dirname = path.resolve();
+  const filePath = path.join(__dirname, 'src/utils/email.html');
+  const source = fs.readFileSync(filePath, 'utf-8').toString();
+  const template = handlebars.compile(source);
+  const resetUrl = `http://localhost:5173/reset-password/${token}`;
+  const replacements = {
+    username: user.fullName,
+    link: resetUrl,
+  };
+  const htmlToSend = template(replacements);
+
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -22,8 +36,6 @@ export const sendMail = async (user: UserFields, token: string) => {
       pass: 'lwkf dumc iqyr rnoq',
     },
   });
-
-  const resetUrl = `http://localhost:5173/reset-password/${token}`;
   const mailOptions = {
     to: user.email,
     from: process.env.EMAIL_USER,
@@ -33,6 +45,7 @@ export const sendMail = async (user: UserFields, token: string) => {
       `Пожалуйста, перейдите по следующей ссылке, чтобы сбросить ваш пароль:\n\n` +
       `${resetUrl}\n\n` +
       `Если вы не запрашивали сброс пароля, просто проигнорируйте это письмо.`,
+    html: htmlToSend,
   };
 
   try {
