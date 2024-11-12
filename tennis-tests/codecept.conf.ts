ea9b18c9efbf30937cc2b277691792f686ceca37
Muhtarov Nurdin <nurdinmuhtarovv@gmail.com>
2024-11-12 20:53:39 +0600

    TC#34 updated the configuration file

--- tennis-tests/codecept.conf.ts
+++ tennis-tests/codecept.conf.ts
@@ -1,13 +1,4 @@
-import { setHeadlessWhen, setCommonPlugins } from '@codeceptjs/configure';
-// turn on headless mode when running with HEADLESS=true environment variable
-// export HEADLESS=true && npx codeceptjs run
-setHeadlessWhen(process.env.HEADLESS);
-
-// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
-setCommonPlugins();
-
-export const config: CodeceptJS.MainConfig = {
-  tests: './*_test.ts',
+exports.config = {
   output: './output',
   helpers: {
     Puppeteer: {
@@ -19,5 +10,43 @@ export const config: CodeceptJS.MainConfig = {
   include: {
     I: './steps_file'
   },
+  mocha: {},
+  bootstrap: null,
+  timeout: null,
+  teardown: null,
+  hooks: [],
+  gherkin: {
+    features: './features/*.feature',
+    steps: ['./step_definitions/steps.ts']
+  },
+  plugins: {
+    screenshotOnFail: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    retryFailedStep: {
+      enabled: true
+    },
+    retryTo: {
+      enabled: true
+    },
+    eachElement: {
+      enabled: true
+    },
+    pauseOnFail: {}
+  },
+  stepTimeout: 0,
+  stepTimeoutOverride: [{
+      pattern: 'wait.*',
+      timeout: 0
+    },
+    {
+      pattern: 'amOnPage',
+      timeout: 0
+    }
+  ],
+  tests: './*_test.ts',
   name: 'tennis-tests'
 }
