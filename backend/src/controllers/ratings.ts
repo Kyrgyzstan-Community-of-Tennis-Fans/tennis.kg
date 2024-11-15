9f1e2d583defc4d773a591dd88611c1f2a7afb8c
Kanat Sydykov <kanatsydykov@MacBook-Air-Kanat.local>
2024-11-15 10:49:51 +0600

    fix sort bug

--- backend/src/controllers/ratings.ts
+++ backend/src/controllers/ratings.ts
@@ -5,6 +5,21 @@ import { Rating } from '../model/Rating';
 
 export const fetchRatings = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const monthMap: Record<string, number> = {
+      january: 1,
+      february: 2,
+      march: 3,
+      april: 4,
+      may: 5,
+      june: 6,
+      july: 7,
+      august: 8,
+      september: 9,
+      october: 10,
+      november: 11,
+      december: 12,
+    };
+
     const ratings = await Rating.find()
       .populate({
         path: 'events',
@@ -12,11 +27,18 @@ export const fetchRatings = async (req: Request, res: Response, next: NextFuncti
           path: 'category',
         },
       })
-      .sort({ year: -1, month: 1 })
       .lean()
       .exec();
 
-    return res.send(ratings);
+    const sortedRatings = ratings.sort((a, b) => {
+      if (a.year !== b.year) {
+        return b.year - a.year;
+      }
+
+      return monthMap[a.month.toLowerCase()] - monthMap[b.month.toLowerCase()];
+    });
+
+    return res.send(sortedRatings);
   } catch (error) {
     return next(error);
   }
