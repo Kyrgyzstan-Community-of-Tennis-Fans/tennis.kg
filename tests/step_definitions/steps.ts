8177359f68842ed6596eeb642de5aa381428c963
nenahui <153425875+nenahui@users.noreply.github.com>
2024-11-17 00:47:35 +0600

    Rating tests (#41)
    * implemented rating tests for regular site users.
    
    * implemented rating tests for regular site users.
    
    * fix bugs #2
    
    ---------
    
    Co-authored-by: Kanat Sydykov <kanatsydykov@MacBook-Air-Kanat.local>

---
+++ tests/step_definitions/steps.ts
@@ -0,0 +1,28 @@
+const { I } = inject();
+
+Given("я нахожусь на странице рейтингов", () => {
+  I.amOnPage("/rating");
+});
+
+When("я нажимаю на аккордеон рейтинга {string}", (ratingCategory) => {
+  I.click(ratingCategory);
+  I.wait(1);
+});
+
+Then("открывается список рейтингов по годам и месяцам", () => {
+  I.seeElement("#accordion");
+});
+
+Then("я вижу список событий, связанных с выбранным рейтингом", () => {
+  I.seeElement("#eventItem");
+});
+
+Then(
+  "я вижу кнопку 'Открыть рейтинг' с атрибутом href, указывающим на Google Docs",
+  () => {
+    I.see("Открыть рейтинг", "#openRatingButton");
+    I.seeElement({
+      css: '#openRatingButton[href*="https://www.google.com"]',
+    });
+  },
+);
