35716b039092fbc00abf5f08ecfe08c40f69a9cd
Gordus Alexander <aldrmiller@gmail.com>
2024-11-17 14:43:33 +0600

    TC#34 Update User Cabinet

--- frontend/src/features/users/PersonalAccount.tsx
+++ frontend/src/features/users/PersonalAccount.tsx
@@ -7,6 +7,13 @@ import { PencilSquareIcon } from '@heroicons/react/24/outline';
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { fetchOneUser } from '@/features/users/usersThunks';
+import GradientCircle from '@/components/GradientCircle/GradientCircle';
+import { GradientCirclesTypes } from '@/types/types';
+
+const gradientCircles: GradientCirclesTypes[] = [
+  { width: '569px', height: '572px', top: '-90px', left: '-272px' },
+  { width: '569px', height: '572px', bottom: '50px', right: '20px' },
+];
 
 export const PersonalAccount: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -22,63 +29,74 @@ export const PersonalAccount: React.FC = () => {
 
   return (
     currentUser && (
-      <Layout>
-        <header className={'flex xs:items-center justify-between gap-2 flex-col xs:flex-row border-b pb-1.5'}>
-          <div>
-            <h1 className={'text-2xl font-medium leading-none'}>Личный кабинет</h1>
-            <small className={'text-muted-foreground text-base'}>
-              Ваша персональная информация и управление данными
-            </small>
-          </div>
+      <>
+        {gradientCircles.map((circle, id) => (
+          <GradientCircle key={id} {...circle} />
+        ))}
 
-          <UserEdit user={currentUser}>
-            <Button>
-              Редактировать
-              <PencilSquareIcon />
-            </Button>
-          </UserEdit>
-        </header>
-
-        <main className='mt-2 flex flex-col max-w-2xl'>
-          <div className='mb-3 flex flex-col'>
-            <h2 className='text-xl font-medium'>{currentUser.fullName}</h2>
-            <span className='text-[#64B32C]'>{currentUser.category.name}</span>
-          </div>
-
-          <div className='space-y-2 flex flex-col'>
-            <div className='flex flex-wrap'>
-              <h3 className='font-medium w-full sm:w-1/3'>Почта</h3>
-              <span className='text-muted-foreground w-full sm:w-2/3 break-words'>{currentUser.email}</span>
-            </div>
-
-            <div className='flex flex-wrap'>
-              <h3 className='font-medium w-full sm:w-1/3'>Телефон</h3>
-              <span className='text-muted-foreground w-full sm:w-2/3'>{currentUser.telephone}</span>
-            </div>
-
-            <div className='flex flex-wrap'>
-              <h3 className='font-medium w-full sm:w-1/3'>День рождения</h3>
-              <span className='text-muted-foreground w-full sm:w-2/3'>{currentUser.dateOfBirth}</span>
-            </div>
-
-            <div className='flex flex-wrap'>
-              <h3 className='font-medium w-full sm:w-1/3'>Пол</h3>
-              <span className='text-muted-foreground w-full sm:w-2/3'>
-                {currentUser.gender === 'male' ? 'Муж.' : 'Жен.'}
-              </span>
-            </div>
-
-            {currentUser.role === 'admin' && (
-              <div className='flex flex-wrap'>
-                <h3 className='font-medium w-full sm:w-1/3'>Роль</h3>
-                <span className='text-muted-foreground w-full sm:w-2/3'>
-                  {currentUser.role === 'admin' ? 'Админ' : 'Пользователь'}
-                </span>
+        <Layout>
+          <div
+            className='bg-white p-[25px] sm:p-[35px] rounded-lg'
+            style={{ boxShadow: '2px 0 89px 0 rgba(0, 0, 0, 0.1)' }}
+          >
+            <header className={'flex xs:items-center justify-between gap-2 flex-col xs:flex-row border-b pb-1.5'}>
+              <div>
+                <h1 className={'text-2xl font-medium leading-none'}>Личный кабинет</h1>
+                <small className={'text-muted-foreground text-base'}>
+                  Ваша персональная информация и управление данными
+                </small>
               </div>
-            )}
+
+              <UserEdit user={currentUser}>
+                <Button>
+                  Редактировать
+                  <PencilSquareIcon />
+                </Button>
+              </UserEdit>
+            </header>
+
+            <main className='mt-2 flex flex-col max-w-2xl'>
+              <div className='mb-3 flex flex-col'>
+                <h2 className='text-xl font-medium'>{currentUser.fullName}</h2>
+                <span className='text-[#64B32C]'>{currentUser.category.name}</span>
+              </div>
+
+              <div className='space-y-2 flex flex-col'>
+                <div className='flex flex-wrap'>
+                  <h3 className='font-medium w-full sm:w-1/3'>Почта</h3>
+                  <span className='text-muted-foreground w-full sm:w-2/3 break-words'>{currentUser.email}</span>
+                </div>
+
+                <div className='flex flex-wrap'>
+                  <h3 className='font-medium w-full sm:w-1/3'>Телефон</h3>
+                  <span className='text-muted-foreground w-full sm:w-2/3'>{currentUser.telephone}</span>
+                </div>
+
+                <div className='flex flex-wrap'>
+                  <h3 className='font-medium w-full sm:w-1/3'>День рождения</h3>
+                  <span className='text-muted-foreground w-full sm:w-2/3'>{currentUser.dateOfBirth}</span>
+                </div>
+
+                <div className='flex flex-wrap'>
+                  <h3 className='font-medium w-full sm:w-1/3'>Пол</h3>
+                  <span className='text-muted-foreground w-full sm:w-2/3'>
+                    {currentUser.gender === 'male' ? 'Муж.' : 'Жен.'}
+                  </span>
+                </div>
+
+                {currentUser.role === 'admin' && (
+                  <div className='flex flex-wrap'>
+                    <h3 className='font-medium w-full sm:w-1/3'>Роль</h3>
+                    <span className='text-muted-foreground w-full sm:w-2/3'>
+                      {currentUser.role === 'admin' ? 'Админ' : 'Пользователь'}
+                    </span>
+                  </div>
+                )}
+              </div>
+            </main>
           </div>
-        </main>
-      </Layout>
+        </Layout>
+      </>
     )
   );
 };
