aec698ffa44661e8faa4f703d4fc14be52a6f10a
AldrDev <122224183+aldr-dev@users.noreply.github.com>
2024-11-19 20:11:04 +0600

    Tc#34 update user cabinet (#47)
    * TC#28-Implement-Background-Design-from-Mockup
    
    * TC#34 Update User Cabinet
    
    * TC#34 change gradient circle from position. And fix theme dark from personal account.

--- frontend/src/components/GradientCircle/GradientCircle.tsx
+++ frontend/src/components/GradientCircle/GradientCircle.tsx
@@ -11,8 +11,8 @@ interface Props {
 }
 
 const GradientCircle: React.FC<Props> = ({ width, height, top, right, left, bottom }) => {
-  const { theme } = useTheme();
-
+const { theme } = useTheme();
+  
   return theme === 'dark' ? null : (
     <div
       className='hidden lg:block 2xl:hidden absolute z-[-9999] opacity-[13%] rounded-full blur-[40px] bg-gradient-to-r from-[rgba(79,173,13,0.34)] via-[#64b32c] to-[rgba(100,179,44,0.26)]'
